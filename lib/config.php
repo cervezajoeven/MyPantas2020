e4943c09e556ce9dab9ffa5a3fcb5c13ae25fad4
Joeven <cervezajoeven@gmail.com>
2020-10-09 09:24:46 +0800

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -2,8 +2,8 @@
 ini_set( "display_errors", true );
 define( "DB_DSN", "mysql:host=localhost;dbname=pantasdbnew" );
 //define( "DB_DSN", "mysql:host=localhost;dbname=pantasdb_latest" );
-define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "divinity" );
+define( "DB_USERNAME", "joeven" );
+define( "DB_PASSWORD", "joeven241" );
 define('DB_CHARACSET', 'utf8');
 
 require_once ('Database.php');
